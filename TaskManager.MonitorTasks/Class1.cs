aa1dc8afdd4855b7bef7489aa3fd6b0609c4b990
creasy-pita <hz_ljq@live.com>
2019-02-22 09:45:39 +0800

    First commit

---
+++ TaskManager.MonitorTasks/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TaskManager.MonitorTasks
+{
+    public class Class1
+    {
+    }
+}
---
+++ TaskManager.Node/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskManager.Node
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
