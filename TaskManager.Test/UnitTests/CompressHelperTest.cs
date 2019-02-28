ae5823edee029ed14bf6924ff604b007c765f5db
creasy-pita <hz_ljq@live.com>
2019-02-28 16:02:37 +0800

    add xunittest project and add CompressHelper .UnCompress method test case ; fix  日志模块不能显示问题： The multi-part identifier "T.taskcreateuserid" could not be bound.

---
+++ TaskManager.Test/UnitTests/CompressHelperTest.cs
@@ -0,0 +1,23 @@
+﻿using TaskManager.Core;
+using Xunit;
+
+namespace TaskManager.Test
+{
+    public class CompressHelperTest
+    {
+        //[Fact]
+        //public void CompressZip_FileCompressToZipAndUnCompressToFile_OutComeSameBytes()
+        //{
+        //    string file = @"E:\work\project\GisqSystem.TaskManager\TaskManager.Test\bin\Debug\netcoreapp2.1\netcoreapp2.0.zip";
+        //    string dir = @"E:\work\project\GisqSystem.TaskManager\TaskManager.Test\bin\Debug\netcoreapp2.1\zip";
+        //    CompressHelper.UnCompress(file, dir);
+        //}
+        [Fact]
+        public void UnCompress_UnCompressConsoleAppZipToDll_CanExecuteRightly()
+        {
+            string file = @"E:\work\project\GisqSystem.TaskManager\TaskManager.Test\bin\Debug\netcoreapp2.1\netcoreapp2.0.zip";
+            string dir = @"E:\work\project\GisqSystem.TaskManager\TaskManager.Test\bin\Debug\netcoreapp2.1\zip";
+            CompressHelper.UnCompress(file, dir);
+        }
+    }
+}
